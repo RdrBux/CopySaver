a78b6a1e57bda76928c70d46ccb58f1836301e15
RdrBux <rodriguezrodrigoemmanuel@gmail.com>
2023-09-19 19:00:41 -0300

    Add default values for badge, db and active status. Add toggler to change active status

--- public/background.js
+++ public/background.js
@@ -1,16 +1,40 @@
-console.log('background.js loaded');
+// Initial setup
+chrome.runtime.onInstalled.addListener(() => {
+  // Create empty data in db in nonexistent
+  chrome.storage.local.get(['data'], function (result) {
+    if (!result.data) {
+      chrome.storage.local.set({ data: [] });
+    }
+    // Update badge
+    chrome.action.setBadgeText({ text: result.data.length.toString() ?? '0' });
+  });
+
+  // Get variable called active from db or create if it doesn't exists
+  chrome.storage.local.get(['active'], function (result) {
+    if (result.active === undefined) {
+      chrome.storage.local.set({ active: true });
+    }
+  });
+});
 
 // check for db updates to change badge
 chrome.storage.onChanged.addListener((changes) => {
+  if (!changes.data) return;
   const dataLength = changes.data.newValue.length;
   chrome.action.setBadgeText({ text: dataLength.toString() });
 });
 
 chrome.runtime.onMessage.addListener((request) => {
-  chrome.storage.local.get(['data'], function (result) {
-    let data = Array.isArray(result.data) ? result.data : [];
-    data = isInStore(data, request.text);
-    chrome.storage.local.set({ data });
+  // Get value of active variable from db
+  chrome.storage.local.get(['active'], function (result) {
+    // Return if active is false
+    if (!result.active) return;
+
+    chrome.storage.local.get(['data'], function (result) {
+      let data = Array.isArray(result.data) ? result.data : [];
+      data = isInStore(data, request.text);
+      chrome.storage.local.set({ data });
+    });
   });
 });
 
--- src/components/Toggler.jsx
+++ src/components/Toggler.jsx
@@ -1,12 +1,28 @@
+import { useEffect, useState } from 'react';
+
 export default function Toggler() {
+  const [isActive, setIsActive] = useState(true);
+
+  useEffect(() => {
+    // get active value from db
+    chrome.storage.local.get(['active'], function (result) {
+      setIsActive(result.active);
+    });
+  });
+
+  function handleChange() {
+    chrome.storage.local.set({ active: !isActive });
+    setIsActive(!isActive);
+  }
+
   return (
     <div className="grid place-content-center">
       <label className="relative inline-flex items-center cursor-pointer">
         <input
-          defaultChecked
           type="checkbox"
-          value=""
+          checked={isActive}
           className="sr-only peer"
+          onChange={handleChange}
         />
         <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-blue-600"></div>
       </label>
